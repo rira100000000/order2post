c044e9b8bcb15e12b31008b7faaa01026f76ef31
rira <101010hayakawa@gmail.com>
2023-07-21 11:07:12 +0900

    tailwindの導入

--- app/frontend/entrypoints/spreadsheet.tsx
+++ app/frontend/entrypoints/spreadsheet.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SpreadSheet from '../../views/spreadsheet/SpreadSheet';
-import '../src/index.css';
+import '../../assets/stylesheets/output.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/views/**/*.{html/erb,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
