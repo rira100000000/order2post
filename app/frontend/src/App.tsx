44e18575aa84c1e4955a5efbd7761866ce84b476
rira <101010hayakawa@gmail.com>
2023-08-07 20:00:09 +0900

    仮トップページの作成

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+      <div className='flex justify-center items-center w-full'>
+        <div className='flex flex-col items-center text-yellow-800'>
+          <h1 className='text-5xl mt-2 mb-2'>CSV変換君</h1>
+          <p className='md:text-xl border-b-8 border-red-500 mt-4 mb-4'>
+            minne,Creemaを使うハンドメイド作家のための発送作業支援サービス！
+          </p>
+          <a
+            href='/users/sign_up'
+            className='text-xl px-4 py-2 mt-2 leading-none border rounded text-amber-500 border-amber-500 hover:border-transparent hover:text-white hover:bg-amber-500'
+          >
+            サインアップ
+          </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
