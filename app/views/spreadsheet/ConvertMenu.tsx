de5dd6838d1cc5df679f0bac847b3de9f60832bb
rira <101010hayakawa@gmail.com>
2023-09-25 14:54:56 +0900

    convertmenuの横幅調整

--- app/views/spreadsheet/ConvertMenu.tsx
+++ app/views/spreadsheet/ConvertMenu.tsx
@@ -57,7 +57,7 @@ export default function ConvertMenu(props: ConvertMenuProps) {
       >
         注文一覧に戻る
       </button>
-      <div className='border border-slate-300 mr-10 ml-10 mt-2'>
+      <div className='border border-slate-300 mx-auto mt-2 w-[50em]'>
         <p className='text-xl font-bold m-2'>内容品設定</p>
         <p className='text-md flex justify-center items-center'>
           クリックポストに表記する内容品を入力してください
@@ -71,7 +71,7 @@ export default function ConvertMenu(props: ConvertMenuProps) {
           lines={props.lines}
         />
       </div>
-      <div className='border border-slate-300 mr-10 ml-10 mt-2'>
+      <div className='border border-slate-300 mx-auto mt-2 w-[50em]'>
         <p className='text-xl font-bold m-2'>内容品個別設定</p>
         <p className='text-md flex justify-center items-center'>
           クリックポストに表記する内容品を入力してください
