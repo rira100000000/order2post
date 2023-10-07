1d2f6f373dc778820f780d50331e30e01f908b04
rira <101010hayakawa@gmail.com>
2023-10-07 18:19:35 +0900

    設定ボタンの実装

--- app/frontend/entrypoints/application.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import Setting from '../../views/spreadsheet/Setting'; // Settingコンポーネントをインポート
-
-const root = createRoot(document.getElementById('setting'));
-root.render(<Setting />);
