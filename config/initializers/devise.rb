0ab086b1bf2e9e5affc3494c800dbef3ebb43c6e
rira <101010hayakawa@gmail.com>
2023-10-10 17:00:42 +0900

    main_appが見つからない不具合の修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -291,7 +291,9 @@ Devise.setup do |config|
   #
   # The router that invoked `devise_for`, in the example above, would be:
   # config.router_name = :my_engine
-  #
+  def main_app
+    Rails.application.class.routes.url_helpers
+  end
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
