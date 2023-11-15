527ecc642e030f6f185f2425b4be0228d6b5764e
rira <101010hayakawa@gmail.com>
2023-11-15 12:58:55 +0900

    Deviseの日本語化

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-module CsvTest
+module CsvConverter
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
