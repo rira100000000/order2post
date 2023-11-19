8edc897ec46ebc548e6eb7f301b83fc2a99403cb
rira <101010hayakawa@gmail.com>
2023-11-19 21:41:12 +0900

    マウスホバーで下線を表示する

--- app/frontend/entrypoints/components/Footer.tsx
+++ app/frontend/entrypoints/components/Footer.tsx
@@ -6,10 +6,14 @@ const Footer = () => {
       <nav className='block p-3 bg-slate-800'>
         <div className='flex justify-center flex-wrap bg-slate-800'>
           <div className='flex text-white mr-6'>
-            <a href='/privacy'>プライバシーポリシー</a>
+            <a href='/privacy' className='hover:underline'>
+              プライバシーポリシー
+            </a>
           </div>
           <div className='flex text-white'>
-            <a href='/terms'>利用規約</a>
+            <a href='/terms' className='hover:underline'>
+              利用規約
+            </a>
           </div>
         </div>
         <div className='flex justify-center text-white mt-2'>
