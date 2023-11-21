8007a50703b5fba360b1630242ba4192bac5c60f
rira <101010hayakawa@gmail.com>
2023-11-21 15:47:36 +0900

    モーダルの位置調整

--- app/frontend/entrypoints/hooks/useModal.tsx
+++ app/frontend/entrypoints/hooks/useModal.tsx
@@ -15,36 +15,9 @@ const useModal: any = () => {
   const Modal = ({ children }: any) => {
     if (!show) return null;
     return (
-      <div
-        style={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <div
-          style={{
-            position: 'fixed',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            backgroundColor: 'gray',
-            opacity: '0.5'
-          }}
-        ></div>
-        <div
-          style={{
-            position: 'relative',
-            whiteSpace: 'pre-line',
-            backgroundColor: '#f5f5f5'
-          }}
-        >
+      <div className='absolute top-0 left-0 right-0 bottom-0 flex md:ml-0 ml-10 md:justify-center items-center'>
+        <div className='fixed top-0 bottom-0 left-0 right-0 bg-gray-500 opacity-50'></div>
+        <div className='relative whitespace-pre-line bg-gray-200'>
           {children}
         </div>
       </div>
