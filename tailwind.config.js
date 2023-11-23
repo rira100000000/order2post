60028ec664e44ca7c2cb924f6a795408446e7ba9
rira <101010hayakawa@gmail.com>
2023-11-23 15:17:26 +0900

    CDNを使わないように修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/views/**/*.{html.erb,tsx}'],
+  content: [
+    './app/views/**/*.{html.erb,tsx}',
+    './app/frontend/entrypoints/*.tsx',
+    './app/frontend/entrypoints/**/*.tsx'
+  ],
   theme: {
     extend: {}
   },
