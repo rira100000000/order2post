47660b58dab1d54420f5666ff09751c940b5cd62
rira <101010hayakawa@gmail.com>
2023-12-05 15:46:10 +0900

    非Reactページのヘッダー修正

--- app/frontend/components/Header.tsx
+++ app/frontend/components/Header.tsx
@@ -7,16 +7,44 @@ const Header = ({ current_user_email }) => {
     <nav className='flex items-center p-3 border-b border-t-0 border-l-0 border-r-0 border-amber-600 print_none'>
       <div className='flex justify-center items-center w-full'>
         <div className='md:flex max-w-1325 w-full justify-between'>
-          <div className='flex flex-shrink-0 text-amber-600'>
+          <div className='flex justify-center md:justify-start flex-shrink-0 text-amber-600'>
             <a href='/'>
               <img src={logo} width='280' alt='ロゴ' />
             </a>
           </div>
-          <div className='flex justify-end'>
-            <div className='text-sm'>
-              <strong className='flex justify-end mr-2 md:mt-5 text-amber-600'>
-                {current_user_email.replace(/"/g, '')}
-              </strong>
+          <div className='flex justify-end items-end overflow-hidden'>
+            <div
+              id='menu'
+              className='md:mt-10 mt-2 translate-y-[50px] transition-all'
+            >
+              <div className='flex flex-col md:flex-row'>
+                <strong className='md:flex mt-0 mr-2 text-amber-600'>
+                  {current_user_email.replace(/"/g, '')}
+                </strong>
+                <div className='flex'>
+                  <a
+                    href='/usage'
+                    id='usage_button'
+                    className='inline-block text-sm px-4 py-2 leading-none border rounded text-amber-600 border-amber-600 hover:border-transparent hover:text-white hover:bg-amber-600 lg:mt-0 mr-2'
+                  >
+                    使い方
+                  </a>
+                  <a
+                    href='/users/edit'
+                    id='setting_button'
+                    className='inline-block text-sm px-4 py-2 leading-none border rounded text-amber-600 border-amber-600 hover:border-transparent hover:text-white hover:bg-amber-600 lg:mt-0 mr-2'
+                  >
+                    設定
+                  </a>
+                  <a
+                    href='/users/sign_out'
+                    id='signout_button'
+                    className='inline-block text-sm px-4 py-2 leading-none border rounded text-amber-600 border-amber-600 hover:border-transparent hover:text-white hover:bg-amber-600 lg:mt-0 mr-2'
+                  >
+                    サインアウト
+                  </a>
+                </div>
+              </div>
             </div>
             <button
               id='openButton'
@@ -25,49 +53,42 @@ const Header = ({ current_user_email }) => {
               onClick={() => {
                 document
                   .getElementById('menu')
-                  ?.classList.toggle('translate-x-full');
-                setOpenMenu(!openMenu);
+                  ?.classList.toggle('translate-y-[50px]');
+                document
+                  .getElementById('closeButton')
+                  ?.classList.toggle('hidden');
+                document
+                  .getElementById('openButton')
+                  ?.classList.toggle('hidden');
+              }}
+            >
+              <i id='bars' className='fa-solid fa-bars fa-2x'></i>
+            </button>
+            <button
+              id='closeButton'
+              type='button'
+              className='z-10 hidden'
+              onClick={() => {
+                document
+                  .getElementById('menu')
+                  ?.classList.toggle('translate-y-[50px]');
+
+                document
+                  .getElementById('closeButton')
+                  ?.classList.toggle('hidden');
+                document
+                  .getElementById('openButton')
+                  ?.classList.toggle('hidden');
               }}
             >
               <i
-                id='bars'
-                className={`fa-solid ${
-                  openMenu ? 'fa-bars' : 'fa-xmark'
-                } fa-2x`}
-              ></i>
+                id='xmark'
+                className='fa-solid fa-xmark fa-2x w-[28px]'
+                onClick={() => {
+                  setOpenMenu(!openMenu);
+                }}
+              />
             </button>
-            <ul
-              id='menu'
-              className='fixed top-0 left-0 z-0 w-full translate-x-full bg-amber-600 text-center text-xl font-bold text-white transition-all ease-linear'
-            >
-              <li className='p-3'>
-                <a
-                  href='/usage'
-                  id='usage_button'
-                  className='inline-block text-xl px-4 py-2 leading-none border rounded text-white border-amber-600 hover:border-transparent hover:text-white hover:underline lg:mt-0 mr-2'
-                >
-                  使い方
-                </a>
-              </li>
-              <li className='p-3'>
-                <a
-                  href='/users/edit'
-                  id='setting_button'
-                  className='inline-block text-xl px-4 py-2 leading-none border rounded text-white border-amber-600 hover:border-transparent hover:text-white hover:underline lg:mt-0 mr-2'
-                >
-                  設定
-                </a>
-              </li>
-              <li className='p-3'>
-                <a
-                  href='/users/sign_out'
-                  id='signout_button'
-                  className='inline-block text-xl px-4 py-2 leading-none border rounded text-white border-amber-600 hover:border-transparent hover:text-white hover:underline lg:mt-0 mr-2'
-                >
-                  サインアウト
-                </a>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
