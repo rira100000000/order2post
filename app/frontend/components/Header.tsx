2e11a34f9f08af23dde415ee32eeca15103c3a57
rira <101010hayakawa@gmail.com>
2023-12-05 15:53:58 +0900

    ボタンのサイズを統一

--- app/frontend/components/Header.tsx
+++ app/frontend/components/Header.tsx
@@ -25,21 +25,21 @@ const Header = ({ current_user_email }) => {
                   <a
                     href='/usage'
                     id='usage_button'
-                    className='inline-block text-sm px-4 py-2 leading-none border rounded text-amber-600 border-amber-600 hover:border-transparent hover:text-white hover:bg-amber-600 lg:mt-0 mr-2'
+                    className='inline-block text-center w-[97px] text-sm px-4 py-2 leading-none border rounded text-amber-600 border-amber-600 hover:border-transparent hover:text-white hover:bg-amber-600 lg:mt-0 mr-2'
                   >
                     使い方
                   </a>
                   <a
                     href='/users/edit'
                     id='setting_button'
-                    className='inline-block text-sm px-4 py-2 leading-none border rounded text-amber-600 border-amber-600 hover:border-transparent hover:text-white hover:bg-amber-600 lg:mt-0 mr-2'
+                    className='inline-block text-center w-[97px] text-sm px-4 py-2 leading-none border rounded text-amber-600 border-amber-600 hover:border-transparent hover:text-white hover:bg-amber-600 lg:mt-0 mr-2'
                   >
                     設定
                   </a>
                   <a
                     href='/users/sign_out'
                     id='signout_button'
-                    className='inline-block text-sm px-4 py-2 leading-none border rounded text-amber-600 border-amber-600 hover:border-transparent hover:text-white hover:bg-amber-600 lg:mt-0 mr-2'
+                    className='inline-block text-center w-[97px] text-sm px-4 py-2 leading-none border rounded text-amber-600 border-amber-600 hover:border-transparent hover:text-white hover:bg-amber-600 lg:mt-0 mr-2'
                   >
                     サインアウト
                   </a>
